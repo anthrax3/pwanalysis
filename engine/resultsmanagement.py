cd6803e2376b47d6b8a76dbd96f27aea58dd577b
Caleb Shortt <cshortt@uvic.ca>
2018-02-21 20:02:38 -0800

    Added basic skeleton of results engine

---
+++ engine/resultsmanagement.py
@@ -0,0 +1,19 @@
+
+
+class ResultsManager(object):
+
+    results = None
+
+    def __init__(self, *args, **kwargs):
+        self.results = {}
+
+    def add_result(self, results):
+        """
+        Adds the given result dictionary to the result-set
+
+        :param result: dictionary -- results
+        :return:
+        """
+
+        pass
+
