608d36a25d8eec8c3be1faa33ad072be5ae3e160
Caleb Shortt <cshortt@uvic.ca>
2018-03-01 19:13:13 -0800

    Added feature parity with results manager

--- engine/resultsmanagement.py
+++ engine/resultsmanagement.py
@@ -1,19 +1,50 @@
 
+from collections import Counter
+
 
 class ResultsManager(object):
+    """
+    Result storage class that can be passed between analysis modules and queried for stored results
+    """
 
     results = None
+    loaded_keys = None
 
-    def __init__(self, *args, **kwargs):
-        self.results = {}
+    def __init__(self, results=None, keys=None):
+
+        self.results = results if results else {}
+        self.loaded_keys = keys if keys else []
 
     def add_result(self, results):
         """
         Adds the given result dictionary to the result-set
 
-        :param result: dictionary -- results
+        :param results: dictionary -- results
         :return:
         """
 
-        pass
+        for key in self.loaded_keys:
+            ledger_mod_results = self.results.get(key, None)
+            new_mod_results = results.get(key, {})
+            if not ledger_mod_results:
+                self.results[key] = new_mod_results
+                continue
+
+            # Add each new result set from the associated analytics function to the ledger
+            for analytics_func, analytics_dict in new_mod_results.items():
+                self.results[key][analytics_func] = dict(Counter(self.results[key][analytics_func]) +
+                                                         Counter(analytics_dict))
+
+    def print_top_ngrams(self):
+        """
+        Prints the top 10 ngrams for each size of ngram in the given dataset
+
+        :return:
+        """
+
+        print('Calculating top ngrams...')
+        max_len = len(sorted(self.results['Frequency Analysis']['pass_ngram_freqs'].keys(), key=lambda x: -len(x))[0])
+        for i in range(1, max_len+1):
+            tmp = {k: v for k, v in self.results['Frequency Analysis']['pass_ngram_freqs'].items() if len(k) == i}
+            print('%2d: %s' % (i, sorted(tmp.items(), key=lambda x: -x[1])))
 
--- pwanalysis.py
+++ pwanalysis.py
@@ -2,10 +2,9 @@
 import argparse
 import logging
 
-from collections import Counter
-
 from settings import MODES, CONSTANTS
 from analytics.base import AnalysisEngine
+from engine.resultsmanagement import ResultsManager
 
 
 logger = logging.getLogger(__name__)
@@ -25,7 +24,8 @@ class Engine(object):
     block_size = None
 
     analyzer = None
-    results = {}
+    results_manager = None
+    # results = {}
 
     def __init__(self, mode, filepath, block_size=None):
         self.mode = mode
@@ -41,6 +41,7 @@ class Engine(object):
         pw_parser = PWDumpParser(filepath=self.filepath, mode=self.mode)
 
         self.analyzer = AnalysisEngine(mode=self.mode)
+        self.results_manager = ResultsManager(keys=self.analyzer.get_loaded_modules())
 
         print('Starting Analysis Engine...')
 
@@ -48,63 +49,10 @@ class Engine(object):
         for block in pw_parser.get_pw_block(self.block_size):
             print('Executing block (size=%s) number %s' % (self.block_size, counter))
             r = self.analyzer.run_analysis_modules(block)
-            self._merge_results(r)
+            self.results_manager.add_result(r)
             counter += 1
 
-        self._print_top_ngrams()
-
-    def _merge_results(self, new_results):
-        """
-        Iterates through loaded modules (names are keys in results) and adds the results to the general results ledger.
-        Each module has multiple results dicts from their various analytics functions
-
-        Example:
-            keys = ['module1', 'module2']
-
-            results {
-                'module1': {
-                    'analytics1': {...}
-                    'analytics2': {...}
-                    ...
-                },
-                'module2': {
-                    ...
-                },
-                ...
-            }
-
-        NOTE: This method allows for combination of results from files that are too large to store im memory.
-
-        :param new_results:
-        :return:
-        """
-        keys = self.analyzer.get_loaded_modules()
-
-        for key in keys:
-            ledger_mod_results = self.results.get(key, None)
-            new_mod_results = new_results.get(key, {})
-            if not ledger_mod_results:
-                self.results[key] = new_mod_results
-                continue
-
-            # Add each new result set from the associated analytics function to the ledger
-            for analytics_func, analytics_dict in new_mod_results.items():
-                self.results[key][analytics_func] = dict(Counter(self.results[key][analytics_func]) +
-                                                         Counter(analytics_dict))
-
-    def _print_top_ngrams(self):
-        """
-        Prints the top 10 ngrams for each size of ngram in the given dataset
-        
-        :return:
-        """
-
-        print('Calculating top ngrams...')
-        max_len = len(sorted(self.results['Frequency Analysis']['pass_ngram_freqs'].keys(), key=lambda x: -len(x))[0])
-        for i in range(1, max_len+1):
-            tmp = {k: v for k, v in self.results['Frequency Analysis']['pass_ngram_freqs'].items() if len(k) == i}
-            # print('%2d: %s' % (i, sorted(tmp.items(), key=lambda x: -x[1])[:10]))
-            print('%2d: %s' % (i, sorted(tmp.items(), key=lambda x: -x[1])))
+        self.results_manager.print_top_ngrams()
 
 
 if __name__ == "__main__":
